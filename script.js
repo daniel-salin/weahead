100fe35befcf60d8cbb6c629292a77f14a33108e
daniel-salin <daniel.salin@chasacademy.se>
2019-04-24 11:00:42 +0200

    :construction: Time alloted is displayed in report table
    -Form validation required
    -Remove example data upon loading DOM

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
-let form = document.querySelector('.report');
+let form = document.querySelector(".report");
+let table = document.querySelector("table");
+
+//-------------------------
+// TIME SUM
+//-------------------------
+
+/*
+Loop over table row and use counter to store
+time data (subtract start time from end time)
+and render in last table row
+*/
+function sumTime() {
+  let timeSum = 0;
+  let hours = 0
+  let minutes = 0
+  // Summarize time over table rows
+  for(let i = 1; i < table.rows.length-1; i++) {
+    let start = table.rows[i].cells[2].innerText;
+    let end = table.rows[i].cells[3].innerText;
+    startArray = start.split(":");
+    endArray = end.split(":");
+    let startHours = parseInt(startArray[0]);
+    let startMinutes = parseInt(startArray[1]);
+    let endHours = parseInt(endArray[0]);
+    let endMinutes = parseInt(endArray[1]);
+    hours += endHours - startHours;
+    minutes += endMinutes - startMinutes;
+    (minutes.toString().length < 2)
+    ? timeSum = ((hours) + ":0" + (minutes))
+    : timeSum = (hours) + ":" + (minutes);
+  }
+  // Insert calculated time into DOM
+  table.rows[table.rows.length-1].cells[0].innerHTML = "Summa total tid: " + timeSum;
+}
+
+//
 
 //-------------------------
 // FORM SUBMISSION
@@ -10,19 +46,23 @@ if(form.addEventListener){
     form.addEventListener("submit", onSubmit, false);  
 }else if(form.attachEvent){
   //Old Browser (IE) fallback
-    form.attachEvent('onsubmit', onSubmit);            
+    form.attachEvent("onsubmit", onSubmit);            
 }
 
 // FORM DATA HANDLING CALLBACK
 function storeData(project) {
-  //Form Data
+  /* Form validation of inserted values would go here 
+    Time would need to be displayed against the correct 
+    time format. Note length and activity as well.
+  */
+
   let activity = document.querySelector("select[name='activity']").value;
+
   let timeStart = document.querySelector("input[name='from']").value;
   let timeEnd = document.querySelector("input[name='to']").value;
   let note = document.querySelector("textarea[name='note']").value;
   let isBillable = document.querySelector("input[name='billable']").checked;
   
-  let table = document.querySelector("table");
   let newRow = table.insertRow(1);
   newRow.insertCell(0).innerHTML = project;
   newRow.insertCell(1).innerHTML = activity;
@@ -31,6 +71,8 @@ function storeData(project) {
   newRow.insertCell(4).innerHTML = note;
   isBillable? newRow.insertCell(5).innerHTML = "Billable" 
   : newRow.insertCell(5).innerHTML = "Not Billable";
+
+  sumTime();
 }
 
 // FORM SUBMISSION EVENT
@@ -40,3 +82,29 @@ function onSubmit(e) {
   // Checking if selected project is valid
   (project !== "")? storeData(project): alert("Please select a project");
 }
+
+
+//-------------------------
+// EDIT ROW
+//-------------------------
+
+/* Insert new table row under the clicked row
+and allow user to fill in new data using above table 
+data as values for new row. Upon submission
+replace old row with new row
+*/
+
+//-------------------------
+// DELETE ROW
+//-------------------------
+
+// Target current row and delete upon button press
+
+
+function init() {
+  sumTime();
+}
+
+window.onload = init();
+
+
